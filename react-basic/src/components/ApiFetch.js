be923e22a3edd192b8da2c773837334de3f47dd0
tomizawa0214 <j.tomizawa.japan@gmail.com>
2021-04-24 02:10:35 +0900

    APIデータ取得(axios+fetch):id

--- react-basic/src/components/ApiFetch.js
+++ react-basic/src/components/ApiFetch.js
@@ -1,9 +1,15 @@
 import React, {useState, useEffect} from 'react'
-import axios from 'axios'
+// import axios from 'axios'
 
 const ApiFetch = () => {
 
     const [posts, setPosts] = useState([])
+    const [id, setId] = useState(400040)
+    const [clicked, setClicked] = useState(false)
+
+    const handlerClicked = () => {
+        setClicked(!clicked)
+    }
 
     useEffect(() => {
         // axios.get('https://jsonplaceholder.typicode.com/posts')
@@ -11,20 +17,25 @@ const ApiFetch = () => {
         //     setPosts(res.data)
         // })
 
-        fetch('https://jsonplaceholder.typicode.com/posts',{method: 'GET'})
+        fetch(`https://weather.tsukumijima.net/api/forecast/city/${id}`,{method: 'GET'})
         .then(res => res.json())
         .then(data => {
             setPosts(data)
         })
-    },[])
+    },[clicked])
 
     return (
         <div>
-            <ul>
+            <input type='text' value={id} onChange={evt=>setId(evt.target.value)} />
+            <button type='button' onClick={handlerClicked}>Get post</button>
+            <br />
+            {posts.title}
+
+            {/* <ul>
                 {
                     posts.map(post => <li key={post.id}> {post.title} </li>)
                 }
-            </ul>
+            </ul> */}
         </div>
     )
 }
