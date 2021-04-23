c8dc7db5e4fb4b2525818895ca61d26cd16343cd
tomizawa0214 <j.tomizawa.japan@gmail.com>
2021-04-24 01:40:25 +0900

    UseEffect(Cleanup)

--- react-basic/src/App.js
+++ react-basic/src/App.js
@@ -1,7 +1,8 @@
 import logo from './logo.svg';
 import './App.css';
 // import Basic2 from './components/Basic2'
-import BasicUseEffect from './components/BasicUseEffect'
+// import BasicUseEffect from './components/BasicUseEffect'
+import TimerContainer from './components/TimerContainer'
 
 function App() {
   return (
@@ -9,7 +10,9 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         {/* <Basic2 /> */}
-        <BasicUseEffect />
+        {/* <BasicUseEffect /> */}
+        <TimerContainer />
+
       </header>
     </div>
   );
