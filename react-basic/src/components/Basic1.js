012ca1490069910901414ba4fc53c5617da85141
tomizawa0214 <j.tomizawa.japan@gmail.com>
2021-04-24 00:56:23 +0900

    イベントハンドラー

--- react-basic/src/components/Basic1.js
+++ react-basic/src/components/Basic1.js
@@ -1,8 +1,12 @@
 import React from 'react'
 
 const Basic1 = (props) => {
+    const clickHandler = () => {
+        console.log('clicked')
+    }
     return (
         <>
+            <button onClick={clickHandler}>Click</button>
             <h1>Hello { props.name }</h1>
             <h1>Hello React2</h1>
         </>
