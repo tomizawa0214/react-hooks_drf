ce3accab7bf9f6df81339682fc8241487b3e9d9f
tomizawa0214 <j.tomizawa.japan@gmail.com>
2021-04-24 00:50:16 +0900

    Functionalコンポーネント

---
+++ react-basic/src/components/Basic1.js
@@ -0,0 +1,12 @@
+import React from 'react'
+
+const Basic1 = (props) => {
+    return (
+        <>
+            <h1>Hello React1</h1>
+            <h1>Hello React2</h1>
+        </>
+    )
+}
+
+export default Basic1
