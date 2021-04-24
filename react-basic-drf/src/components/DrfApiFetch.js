618ede6a166a963b4d5bcfb28c2e539c90f4daa8
tomizawa0214 <j.tomizawa.japan@gmail.com>
2021-04-24 15:35:51 +0900

    SPA:DELETE Method

--- react-basic-drf/src/components/DrfApiFetch.js
+++ react-basic-drf/src/components/DrfApiFetch.js
@@ -25,19 +25,24 @@ const DrfApiFetch = () => {
         })
     } 
 
-    const deleteTask = () => {
+    const deleteTask = (id) => {
         axios.delete(`http://127.0.0.1:8000/api/tasks/${id}/`,{
             headers: {
                 'Authorization': 'Token 21338bbcd570dc39e263fc8e475bc009c4b6bc82'
             }})
-            .then(res => console.log(res))
+            .then(res => {setTasks(tasks.filter(task => task.id !== id)); setSelectedTask([])})
     } 
 
     return (
         <div>
             <ul>
                 {
-                    tasks.map(task => <li key={task.id}> {task.title}  {task.id} </li>)
+                    tasks.map(task => <li key={task.id}> {task.title}  {task.id} 
+                    <button onClick={()=>deleteTask(task.id)}>
+                        <i className="fas fa-trash-alt"></i>
+                    </button>
+
+                    </li>)
                 }
             </ul>
 
@@ -45,7 +50,7 @@ const DrfApiFetch = () => {
             <input type='text' value={id} onChange={evt=>{setId(evt.target.value)}}/>
             <br />
             <button type='button' onClick={()=>getTask()}>Get task</button>
-            <button type='button' onClick={()=>deleteTask()}>Delete task</button>
+            {/* <button type='button' onClick={()=>deleteTask()}>Delete task</button> */}
             <h3>{selectedTask.title} {selectedTask.id}</h3>
         </div>
     )
